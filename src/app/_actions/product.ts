08b392b470b85ffc112aca0d767d1e2fd95d224d
ashikur37 <piash3700@gmail.com>
2023-07-12 23:23:41 +0600

    multiple categories

--- src/app/_actions/product.ts
+++ src/app/_actions/product.ts
@@ -1,27 +1,58 @@
 "use server"
 
 import { revalidatePath } from "next/cache"
+import slug from "slug"
 import { z } from "zod"
-import slug from 'slug';
+
 import { deleteImageFromCloudinary } from "@/lib/cloudinary"
 import { db } from "@/lib/db"
 import { ProductSchema } from "@/lib/validations/product"
 
-interface AddProductActionType extends z.infer<typeof ProductSchema>{
-  image:string,
-  images:string[],
+interface AddProductActionType extends z.infer<typeof ProductSchema> {
+  image: string
+  images: string[]
 }
-export async function addProductAction(input:AddProductActionType ) {
-  const res = await db.product.create({
+export async function addProductAction(input: AddProductActionType) {
+  const product = await db.product.create({
     data: {
       name: input.name,
       image: input.image,
       description: input.description,
-      slug:slug(input.name)
+      slug: slug(input.name),
     },
   })
+  if (input.prices) {
+    await db.price.createMany({
+      data: input.prices?.map((price) => ({
+        label: price.label,
+        unitPrice: price.unitPrice!,
+        productId: product.id,
+      })),
+    })
+  }
+  await db.categoriesOnProducts.createMany({
+    data: input.categories?.map((category) => ({
+      categoryId: category,
+      productId: product.id,
+    })),
+  })
+  await db.productImage.createMany({
+    data: input.images?.map((image) => ({
+      image,
+      productId: product.id,
+    })),
+  })
+  //colors
+
+  
+  //sizes
+
+
+  //times
+
+
   revalidatePath("/admin/products")
-  return res.image
+  return product.image
 }
 export async function deleteProductAction(id: number) {
   const product = await db.product.findUnique({
@@ -37,14 +68,14 @@ export async function deleteProductAction(id: number) {
   })
   revalidatePath("/admin/products")
 }
-export async function featureProductAction(id:number,featured:boolean){
+export async function featureProductAction(id: number, featured: boolean) {
   await db.product.update({
-    data:{
-      featured
+    data: {
+      featured,
     },
-    where:{
-      id
-    }
+    where: {
+      id,
+    },
   })
   revalidatePath("/admin/products")
   revalidatePath("/")
--- src/lib/validations/product.ts
+++ src/lib/validations/product.ts
@@ -16,24 +16,28 @@ export const ProductSchema = z.object({
   colors: z.array(
     z.object({
       colorId: z.number().int().positive(),
+      extra: z.number().int().positive(),
+
     })
-  ),
+  ).optional(),
   prices: z.array(
     z.object({
       label: z.string().min(1),
       unitPrice: z.number().positive(),
     })
-  ),
+  ).optional(),
   sizes: z.array(
     z.object({
       sizeId: z.number().int().positive(),
+      extra: z.number().int().positive(),
+
     })
-  ),
+  ).optional(),
   times: z.array(
     z.object({
       quantity: z.string().min(1),
       time: z.string().min(1),
     })
-  ),
+  ).optional(),
   // featured:z.boolean()
 })
