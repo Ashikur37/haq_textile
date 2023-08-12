6eaaec6785c2d88c79e54381685e0455cc46d032
ashikur37 <piash3700@gmail.com>
2023-08-12 17:30:11 +0600

    atrtribute ade

--- src/app/(front)/product/[slug]/page.tsx
+++ src/app/(front)/product/[slug]/page.tsx
@@ -24,6 +24,12 @@ export default async function ProductPage({ params }: PrdouctPageProps) {
                     color: true
                 }
             },
+            attributes:{
+                include:{
+                    attribute:true
+                
+                }
+            },
             sizes: {
                 include: {
                     size: true
@@ -32,65 +38,79 @@ export default async function ProductPage({ params }: PrdouctPageProps) {
         }
     });
     console.log(product?.name);
-    return <div className="container flex gap-4 p-5 flex-col lg:flex-row bg-[#f8f9fa ] leading-7  ">
-        <div className="w-2/5">
-            <ProductSlider thumbnailImage={product?.image!} images={product?.images.map(image => image.image!)} />
-        </div >
-        <div className="w-3/5">
-            <h1 className="text-2xl font-bold">{product?.name}</h1>
-            {/* <p className="text-xl"> ${getMinPrice(product?.prices)} to 
+    return <div className="container">
+        <div className=" flex gap-4 p-5 flex-col lg:flex-row bg-[#f8f9fa ] leading-7  ">
+            <div className="w-2/5">
+                <ProductSlider thumbnailImage={product?.image!} images={product?.images.filter(image => image.image).map(image => image.image!)} />
+            </div >
+            <div className="w-3/5">
+                <h1 className="text-2xl font-bold">{product?.name}</h1>
+                {/* <p className="text-xl"> ${getMinPrice(product?.prices)} to 
                                         ${getMaxPrice(product?.prices)}</p> */}
-            <div className="flex gap-4 ">
-                {
-                    product?.prices.map(price => <div key={price.id}>
-                        <h4>
-                            {price.label}
-                        </h4>
-                        <b>
-                            ${price.unitPrice}
-                        </b>
-                    </div>)
-                }
-            </div>
-            <hr />
-            {
-                product?.colors&& <ProductColor colors={product?.colors} />
-            }
-            <div className="flex gap-4">
-                <h4>
-                    Size
-                </h4>
-                <div className="flex flex-col">
+                <div className="flex gap-4 ">
                     {
-                        product?.sizes.map(size => <div key={size.sizeId} className="flex gap-4">
+                        product?.prices.map(price => <div key={price.id}>
+                            <h4>
+                                {price.label}
+                            </h4>
                             <b>
-                                {size.size.name}
+                                ${price.unitPrice}
                             </b>
-                            <b>
-                                ${size.extra + getMaxPrice(product?.prices)}
-                            </b>
-                            <SizeCart />
                         </div>)
                     }
                 </div>
-            </div>
-            <p className="text-gray-400 ">
-                <Balancer>
-               <Description description={product?.description!}/>
-                    <br />
+                <hr />
+                {
+                    product?.colors && <ProductColor colors={product?.colors} />
+                }
+                <div className="flex gap-4">
                     <h4>
-                        Contact US
+                        Size
                     </h4>
-                    <div className="border-2 border-gray-300 px-8 py-7 leading-9">
-                        <h4>Phone</h4>
-                        <h4><p>+8801721121233</p></h4>
-                        <h4> Office Address</h4>
-                        <p>
-                            Dr.Aminul Islam Mension 1st floor,141,DIT Ext Road, Fakirapool, Dhaka-100</p>
-                        <h4><p>mshaquetextile@gmail.com</p></h4>
+                    <div className="flex flex-col">
+                        {
+                            product?.sizes.map(size => <div key={size.sizeId} className="flex gap-4">
+                                <b>
+                                    {size.size.name}
+                                </b>
+                                <b>
+                                    ${size.extra + getMaxPrice(product?.prices)}
+                                </b>
+                                {/* <SizeCart /> */}
+                            </div>)
+                        }
                     </div>
-                </Balancer>
-            </p>
+                </div>
+                <div>
+                    {product?.attributes.map((attribute)=><div key={attribute.attributeId} className="flex gap-4">
+                    <b>
+                                {attribute.attribute.name}
+                            </b>
+                            <b>
+                                {attribute.value}
+                            </b>
+                    </div>)}
+                </div>
+                <p className="text-gray-400 ">
+                    <Balancer>
+
+                        <br />
+                        <h4>
+                            Contact US
+                        </h4>
+                        <div className="border-2 border-gray-300 px-8 py-7 leading-9">
+                            <h4>Phone</h4>
+                            <h4><p>+8801721121233</p></h4>
+                            <h4> Office Address</h4>
+                            <p>
+                                Dr.Aminul Islam Mension 1st floor,141,DIT Ext Road, Fakirapool, Dhaka-100</p>
+                            <h4><p>mshaquetextile@gmail.com</p></h4>
+                        </div>
+                    </Balancer>
+                </p>
+            </div>
+
         </div>
+        <Description description={product?.description!} />
     </div>
 }
--- src/components/product-page/product-slider.tsx
+++ src/components/product-page/product-slider.tsx
@@ -10,23 +10,24 @@ interface ProductSliderProps {
     thumbnailImage:string
 }
 export default function ProductSlider({ images,thumbnailImage }: ProductSliderProps) {
-    return <Swiper
-        spaceBetween={50}
-        slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
-        pagination={{
-            clickable: true,
-        }}
-        onSwiper={(swiper) => console.log(swiper)}
-    >
-         <SwiperSlide>
-                <Image src={thumbnailImage} alt="" width={1000} height={1200}/>
-            </SwiperSlide>
-        {images?.map((image, index) => (
-            <SwiperSlide key={index}>
-                <Image src={image} alt="" width={1000} height={1200}/>
-            </SwiperSlide>
-        ))}
-    </Swiper>
+    return <>
+    <Swiper
+    spaceBetween={50}
+    slidesPerView={1}
+    onSlideChange={() => console.log('slide change')}
+    pagination={{
+        clickable: true,
+    }}
+    onSwiper={(swiper) => console.log(swiper)}
+>
+     <SwiperSlide>
+            <Image src={thumbnailImage} alt="" width={1000} height={1200}/>
+        </SwiperSlide>
+    {images?.map((image, index) => (
+        <SwiperSlide key={index}>
+            <Image src={image} alt="" width={1000} height={1200}/>
+        </SwiperSlide>
+    ))}
+</Swiper></>
 
 }
